c64f23863e7aba6493e9b3b25e92839eba2ffa51
dharun7405 <dharunsudha74@gmail.com>
2025-07-30 23:21:59 +0530

    Schemas added

--- BackendGO/cmd/api/main.go
+++ BackendGO/cmd/api/main.go
@@ -1,1 +1,45 @@
-package api
+package main
+
+import (
+	"database/sql"
+	"log"
+	_ "rest-api-in-gin/docs"
+	"rest-api-in-gin/internal/database"
+	"rest-api-in-gin/internal/env"
+
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq" // PostgreSQL driver
+)
+
+// @title Go Gin Rest API
+// @version 1.0
+// @description A rest API in Go using Gin framework
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Enter your bearer token in the format **Bearer &lt;token&gt;**
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://postgres:marvel74@localhost:5432/EventApp?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	models := database.NewModels(db)
+	app := &application{
+		port:      env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-123456"),
+		models:    models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
+}
--- BackendGO/go.mod
+++ BackendGO/go.mod
@@ -4,6 +4,7 @@ go 1.24.4
 
 require (
 	github.com/golang-migrate/migrate/v4 v4.18.3
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
 
