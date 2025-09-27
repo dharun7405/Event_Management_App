5b56be13e78d09a5e918e50b19ef903b8689bd67
dharun7405 <dharunsudha74@gmail.com>
2025-09-27 17:37:03 +0530

    Rebooted the project

--- BackendGO/cmd/migrate/migrations/000001_create_users_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-                                     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                                     email TEXT NOT NULL UNIQUE,
-                                     name TEXT NOT NULL,
-                                     password TEXT NOT NULL
-);
--- BackendGO/cmd/migrate/migrations/000003_create_attendees_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS attendees (
-                                         id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                                         user_id INTEGER NOT NULL,
-                                         event_id INTEGER NOT NULL,
-                                         FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-                                         FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
-);
