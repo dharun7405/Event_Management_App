a91e3fa8cce94210ca2d58542527bb63ee16a54a
dharun7405 <dharunsudha74@gmail.com>
2025-09-28 10:56:36 +0530

    connected events with attendees

--- BackendGO/internal/database/attendees.go
+++ BackendGO/internal/database/attendees.go
@@ -1,6 +1,10 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type AttendeeModel struct {
 	DB *sql.DB
@@ -10,4 +14,61 @@ type Attendee struct {
 	Id      int `json:"id"`
 	UserId  int `json:"userId"`
 	EventId int `json:"eventId"`
+}
+
+func (m *AttendeeModel) Insert(attendee *Attendee) (*Attendee, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `INSERT INTO attendees (event_id, user_id) VALUES ($1, $2) RETURNING id`
+	err := m.DB.QueryRowContext(ctx, query, attendee.EventId, attendee.UserId).Scan(&attendee.Id)
+
+	if err != nil {
+		return nil, err
+	}
+	return attendee, nil
+}
+
+func (m *AttendeeModel) GetByEventAndAttendee(eventId, userId int) (*Attendee, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `SELECT * FROM attendees WHERE event_id = $1 AND user_id = $2`
+	var attendee Attendee
+	err := m.DB.QueryRowContext(ctx, query, eventId, userId).Scan(&attendee.Id, &attendee.UserId, &attendee.EventId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &attendee, nil
+}
+
+func (m AttendeeModel) GetAttendeesByEvent(eventId int) ([]User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+     SELECT u.id, u.name, u.email
+     FROM users u
+     JOIN attendees a ON u.id = a.user_id
+     WHERE a.event_id = $1
+ `
+	rows, err := m.DB.QueryContext(ctx, query, eventId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.Id, &user.Name, &user.Email)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
 }
--- BackendGO/internal/database/users.go
+++ BackendGO/internal/database/users.go
@@ -1,6 +1,10 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type UserModel struct {
 	DB *sql.DB
@@ -11,4 +15,33 @@ type User struct {
 	Email    string `json:"email"`
 	Name     string `json:"name"`
 	Password string `json:"-"`
+}
+
+func (m *UserModel) Insert(user *User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING id`
+	err := m.DB.QueryRowContext(ctx, stmt, user.Email, user.Password, user.Name).Scan(&user.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `SELECT * FROM users WHERE id = $1`
+
+	var user User
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(&user.Id, &user.Email, &user.Name, &user.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
 }
