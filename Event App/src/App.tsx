920d87a7f47b6df755ba5b4756e6829a69671c31
dharun7405 <dharunsudha74@gmail.com>
2025-09-30 14:39:09 +0530

    Pages added

--- Event App/src/App.tsx
+++ Event App/src/App.tsx
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { EventsPage } from "./pages/EventsPage"
+import { EventDetailsPage } from "./pages/EventDetailsPage"
+import { MyEventsPage } from "./pages/MyEventsPage"
+import { LoginPage } from "./pages/LoginPage"
+import { RegisterPage } from "./pages/RegisterPage"
+import { EditEventPage } from "./pages/EditEventPage"
 
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600 underline">
-      Event App
-    </h1>
+    <div className="min-h-screen bg-gray-50">
+      <Router>
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<EventsPage />} />
+            <Route path="/my-events" element={<MyEventsPage />} />
+            <Route path="/events/:eventId" element={<EventDetailsPage />} />
+            <Route path="/events/:eventId/edit" element={<EditEventPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
   )
 }
 
