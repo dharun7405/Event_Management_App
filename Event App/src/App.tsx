492fdc81ed1665b08754912e2391744ef8d49e73
dharun7405 <dharunsudha74@gmail.com>
2025-10-01 22:27:57 +0530

    Addind authentication with react context

--- Event App/src/App.tsx
+++ Event App/src/App.tsx
@@ -6,26 +6,29 @@ import { LoginPage } from "./pages/LoginPage"
 import { RegisterPage } from "./pages/RegisterPage"
 import { EditEventPage } from "./pages/EditEventPage"
 import { Header } from "./components/header/Header"
+import { AuthProvider } from "./contexts/AuthProvider"
 
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Router>
-        <Header/>
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<EventsPage />} />
-            <Route path="/my-events" element={<MyEventsPage />} />
-            <Route path="/events/:eventId" element={<EventDetailsPage />} />
-            <Route path="/events/:eventId/edit" element={<EditEventPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </main>
-      </Router>
-    </div>
+    <AuthProvider>
+      <div className="min-h-screen bg-gray-50">
+        <Router>
+          <Header />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<EventsPage />} />
+              <Route path="/my-events" element={<MyEventsPage />} />
+              <Route path="/events/:eventId" element={<EventDetailsPage />} />
+              <Route path="/events/:eventId/edit" element={<EditEventPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </main>
+        </Router>
+      </div>
+    </AuthProvider>
   )
 }
 
--- Event App/src/pages/LoginPage.tsx
+++ Event App/src/pages/LoginPage.tsx
@@ -1,3 +1,7 @@
+import { useAuth } from "../contexts/AuthContext"
+
 export function LoginPage() {
+    const auth = useAuth();
+    console.log(auth);
     return <div>LoginPage</div>
 }
