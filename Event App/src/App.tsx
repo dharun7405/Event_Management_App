e6c57a5fde81149828173df4089c5b16f7c990fc
dharun7405 <dharunsudha74@gmail.com>
2025-10-01 21:40:57 +0530

    Header component added

--- Event App/src/App.tsx
+++ Event App/src/App.tsx
@@ -5,12 +5,14 @@ import { MyEventsPage } from "./pages/MyEventsPage"
 import { LoginPage } from "./pages/LoginPage"
 import { RegisterPage } from "./pages/RegisterPage"
 import { EditEventPage } from "./pages/EditEventPage"
+import { Header } from "./components/header/Header"
 
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Router>
+        <Header/>
         <main className="container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<EventsPage />} />
