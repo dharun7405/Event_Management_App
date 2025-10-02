e346ca23730ac5b8220eb536109f1adf78aa6245
dharun7405 <dharunsudha74@gmail.com>
2025-10-02 16:36:50 +0530

    Error and loading components added

---
+++ Event App/src/components/LoadingMessage.tsx
@@ -0,0 +1,9 @@
+export function LoadingMessage({ message }: { message: string }) {
+    return (
+        <div className="flex justify-center py-10">
+            <div className="animate-pulse text-gray-500">
+                {message}
+            </div>
+        </div>
+    )
+}
