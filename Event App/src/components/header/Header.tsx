412793aa89cc66a7b0f9a862ad6c1fa2a384fce7
dharun7405 <dharunsudha74@gmail.com>
2025-10-02 19:59:28 +0530

    Logout handled

--- Event App/src/components/header/Header.tsx
+++ Event App/src/components/header/Header.tsx
@@ -1,5 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useAuth } from "../../contexts/AuthContext"
+import { Button } from "../Button";
+
 export function Header() {
+    const { isAuthenticated, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/");
+    };
+
     return (
         <header className="flex items-center justify-between bg-white p-4 shadow">
             <h2 className="text-xl font-bold">Event app</h2>
@@ -11,12 +22,21 @@ export function Header() {
                     <li>
                         <Link to="/my-events">My Events</Link>
                     </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/register">Register</Link>
-                    </li>
+                    {isAuthenticated ? (
+                        <li>
+                            <Button variant="secondary" size="small" onClick={handleLogout}>
+                                Logout</Button>
+                        </li>
+                    ) : (
+                        <>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/register">Register</Link>
+                            </li>
+                        </>)}
+
                 </ul>
             </nav>
         </header>
