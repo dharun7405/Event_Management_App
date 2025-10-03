871ee1567f055a1b2a8cf9c959a42eb9af027113
dharun7405 <dharunsudha74@gmail.com>
2025-10-03 14:36:13 +0530

    Create enent handled

--- Event App/src/pages/EventsPage.tsx
+++ Event App/src/pages/EventsPage.tsx
@@ -2,20 +2,47 @@ import { fetchEvents } from "../api/events";
 import { EventList } from "../components/events/EventList";
 import type { EventData } from "../types";
 import { useFetch } from "../hooks/useFetch";
+import { AddEventModal } from "../components/events/AddEventModal";
+import { useAuth } from "../contexts/AuthContext";
+import { useState } from "react";
+import { Button } from "../components/Button";
+import { PlusIcon } from "lucide-react";
 
 export function EventsPage() {
-    const { data, loading, error } = useFetch<EventData[]>(fetchEvents);
+    const { isAuthenticated } = useAuth();
+    const [isAddEventModalOpen, setIsAddEventModalOpen] = useState(false);
+
+    const { data, loading, error, refetch } = useFetch<EventData[]>(fetchEvents);
     const events = data ?? [];
+
+    const handleAddEvent = () => {
+        refetch();
+        setIsAddEventModalOpen(false);
+    }
 
     return (
         <>
             <div className="mb-6 flex items-center justify-between">
                 <h1 className="text-2xl font-semibold text-gray-800">Events</h1>
             </div>
+            <div className="flex items-center space-x-4">
+                {isAuthenticated && (
+                    <Button onClick={() => setIsAddEventModalOpen(true)}
+                        icon={<PlusIcon className="mr-1 h-4 w-4" />}>
+                        Add Event
+                    </Button>
+                )}
+            </div>
 
             <EventList loading={loading}
                 error={error?.message ?? null}
                 events={events} />
+
+            {isAddEventModalOpen && isAuthenticated && (
+                <AddEventModal isOpen={isAddEventModalOpen}
+                    onClose={() => setIsAddEventModalOpen(false)}
+                    onAddEvent={handleAddEvent} />
+            )}
         </>
     );
 }
